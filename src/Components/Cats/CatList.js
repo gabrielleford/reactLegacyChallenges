b6caa3dd84acf3e20e49b285fb727764d57308f1
Gabrielle Ford <gabrielledeva@gmail.com>
2022-01-11 12:34:08 -0500

    finished cat challenge

--- src/Components/Cats/CatList.js
+++ src/Components/Cats/CatList.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
- 
-const CatList = () => {
- return (
-   <div>
-     {props.breeds.Map(cat => <li>cat</li> )}
-   </div>
- )
+
+export default class CatList extends Component {
+
+  render() {
+    return (
+      <div>
+        {this.props.cats.map((cat) => (
+          <li>{cat}</li>
+        ))}
+      </div>
+    );
+  }
 }
- 
-CatList;
