814ff7640da76b21245eb02d2018ad80d4aaf3ae
Gabrielle Ford <gabrielledeva@gmail.com>
2022-01-11 19:06:14 -0500

    finished todo list

--- src/Components/ToDo/ToDoIndex.js
+++ src/Components/ToDo/ToDoIndex.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import {AiOutlineCheckCircle} from 'react-icons/ai';
 
 export default class ToDoIndex extends Component {
     constructor(props) {
@@ -10,6 +11,7 @@ export default class ToDoIndex extends Component {
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.removeTask = this.removeTask.bind(this);
     }
 
     handleChange(e) {
@@ -24,6 +26,12 @@ export default class ToDoIndex extends Component {
         })
     }
 
+    removeTask(task) {
+        this.setState({
+            toDoList: this.state.toDoList.filter(item => item !== task)
+        })
+    }
+
     render() {
         return (
             <div>
@@ -32,29 +40,26 @@ export default class ToDoIndex extends Component {
                     <input placeholder="Task" onChange={e => this.handleChange(e)} />
                     <button type="submit">Create New Task</button>
                 </form>
-                <Tasks toDoList={this.state.toDoList} />
+                <Tasks toDoList={this.state.toDoList} removeTask={this.removeTask}/>
             </div>
         )
     }
 }
 
 class Tasks extends Component {
+
     render() {
         return (
             <div>
                 {this.props.toDoList.map((task, index) => {
-                    return <SingleTask newTask={task} key={index} />
+                    return (
+                        <div key={index} onClick={() => this.props.removeTask(task)}>
+                            <h4><AiOutlineCheckCircle /> {task}</h4>
+                        </div>
+                    )
+                    // return <SingleTask checkToDoList={this.props.toDoList} newTask={task} key={index} id={index} />
                 })}
             </div>
         )
     }
-}
-
-const SingleTask = (props) => {
-    return (
-        <>
-        <h3>{props.newTask}</h3>
-        <input type="checkbox" />
-        </>
-    )
 }
